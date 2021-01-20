952a6bdf311352bd76e410f4ca9dc5b32808e127
Stefany Sanchez <stefanysanz@icloud.com>
2021-01-19 23:18:37 -0800

    css changes

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
 const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.JAWSDB_URL || 'mysql://root:NUcode2020!114@localhost/burgers_db')
+const connection = mysql.createConnection(process.env.JAWSDB_URL || 'mysql://root:root@localhost/burgers_db')
 
 module.exports = connection
--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,6 @@
 USE burgers_db;
 
 INSERT INTO burgers (burger_name, devoured)
-VALUES ('Five Guys Burger', false), 
-('Five Guys Cheeseburger', true), 
-('Five Guys Bacon Cheeseburger', false);
+VALUES ('Impossible Burger', false), 
+('Bacon Cheeseburger', true), 
+('Double Bacon Cheeseburger', false);
